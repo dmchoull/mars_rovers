c0ddd95ab170d91968d8b95361bf13a24813067f
David <dmchoull@gmail.com>
2019-03-19 18:18:20 -0400

    add files for the sample input/output given in the problem description

--- src/index.js
+++ src/index.js
@@ -1,42 +1,24 @@
 const fs = require("fs");
 const { promisify } = require("util");
 const stat = promisify(fs.stat);
+const readFile = promisify(fs.readFile);
 
-async function validateInputFile(inputFile) {
-  if (!(typeof inputFile === "string")) {
-    throw new Error(`Invalid value "${inputFile}" for input file. Please provide a path to an input file.`);
-  }
+const argv = require("yargs").option("f", {
+  alias: "file",
+  describe: "path to the input file",
+  type: "string",
+  nargs: 1,
+  default: "./input.txt",
+  requiresArg: true,
+}).argv;
 
+(async function run(inputFile) {
   try {
     await stat(inputFile);
   } catch (e) {
     throw new Error(`Problem reading input file from ${inputFile}`);
   }
-}
 
-require("yargs").command(
-  "run [file]",
-  "execute with the given input file",
-  yargs => {
-    yargs.positional("file", {
-      describe: "path to the input file",
-      default: "./input.txt",
-    });
-  },
-  argv => {
-    const inputFile = argv.file;
-    validateInputFile(inputFile).then(() => run(inputFile));
-  },
-).argv;
-
-
-function run(inputFile) {
-  console.log(`reading ${inputFile}`);
-
-  fs.readFile(inputFile, (err, data) => {
-    if (err) {
-      console.error(`error happened: ${err.toString()}`);
-    }
-    console.log(`read: ${data}`);
-  });
-}
+  const data = await readFile(inputFile);
+  console.log(data.toString());
+})(argv.file);
