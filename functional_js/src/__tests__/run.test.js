baebae91c32ee11801ef0434808e537b669eb1cd
David <dmchoull@gmail.com>
2019-03-27 23:55:19 -0400

    add more test input files

--- functional_js/src/__tests__/run.test.js
+++ functional_js/src/__tests__/run.test.js
@@ -2,7 +2,7 @@ import "@babel/polyfill";
 import { run } from "../run";
 
 test("returns rendered output using the given renderer", async () => {
-  const output = await run("./fixtures/sample-input.txt", JSON.stringify);
+  const output = await run("./fixtures/sample.txt", JSON.stringify);
 
   expect(output).toMatchInlineSnapshot(
     `"{\\"rovers\\":[{\\"id\\":1,\\"orientation\\":\\"N\\",\\"coordinates\\":{\\"x\\":1,\\"y\\":3}},{\\"id\\":2,\\"orientation\\":\\"E\\",\\"coordinates\\":{\\"x\\":5,\\"y\\":1}}]}"`
